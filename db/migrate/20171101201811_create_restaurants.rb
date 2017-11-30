f50c76bece8b0fba779eb3ba2433ce542a5878b1
Andy Carlson <andy.carlson4@gmail.com>
2017-11-30 11:02:11 -0500

    reserveme

---
+++ db/migrate/20171101201811_create_restaurants.rb
@@ -0,0 +1,12 @@
+class CreateRestaurants < ActiveRecord::Migration[5.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :description
+      t.string :phone
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
